4f120a7885648646cde75454298bda84f1a9d0d3
GeoDaz <gfrydz2@gmail.com>
2024-07-23 22:35:58 +0200

    updates

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## To transform a CSV file to a JSON file
+```
+$ cd src
+$ node csvtojson.js {filename without extension}
+```
+
+# If it was a rounds file, them run the following command
+```
+$ node rounds.js {filename without extension}
+```
--- src/rounds.js
+++ src/rounds.js
@@ -3,11 +3,14 @@ const { writeJson, readJson } = require('./lib');
 const src = './json/' + process.argv[2] + '.json';
 const datas = readJson(src);
 
-const rounds = [];
+const rounds = {};
 
 datas.forEach(data => {
 	if (data.round == undefined) return;
-	if (!rounds[data.round]) rounds[data.round] = { W: [], L: [] };
+	if (!rounds[data.round]) rounds[data.round] = {};
+	if (data.bracket == 'W') data.bracket = 'Winner Bracket';
+	if (data.bracket == 'L') data.bracket = 'Loser Bracket';
+	if (!rounds[data.round][data.bracket]) rounds[data.round][data.bracket] = [];
 	rounds[data.round][data.bracket].push(data);
 });
 
